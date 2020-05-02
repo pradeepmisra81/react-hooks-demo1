fb621c871ff77275a616a42e4de55ebe5766156d
pradeep misra <pradeep.misra@kognitiv.com>
2020-05-02 17:31:17 +0530

    created react app

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Welcome to React Hooks world !
+        </p>
+        <a
+        >
+          Learn React and React Hooks
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
